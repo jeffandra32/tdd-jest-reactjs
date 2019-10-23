3c0c9eb3bab179bf546fd12284ef02e388913fa3
jeffersonandrade <jefferson@voxtecnologia.com.br>
2019-10-23 15:38:16 -0300

    [ADD] - Projeto TDD Jest

---
+++ __tests__/store/reducers/Techs.test.js
@@ -0,0 +1,16 @@
+import * as Techs from '~/store/modules/techs/actions';
+import reducer, { INITIAL_STATE } from '~/store/modules/techs/reducer';
+
+describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
+  it('ADD_TECH', () => {
+    const state = reducer(INITIAL_STATE, Techs.addTech('Node.js'));
+
+    expect(state).toStrictEqual(['Node.js']);
+  });
+});
